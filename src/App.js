3bb9c7a1bcd2ce6e6029337fff9c2def929d1777
miura <sloth1380.info@gmail.com>
2021-10-13 00:39:08 +0900

    15-component class Component → function Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    const greeting = "Hi!";
-    
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => console.log(1)} />
-      </React.Fragment>
-    )
-    //<input type="text" onChange={()=> console.log(1)} />
-  }
+
+const App = () => {
+  return(
+    <React.Fragment>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </React.Fragment>
+  )
 }
+
+const Cat = () => {
+  return <div>Meow!</div>
+}
+//class App extends Component {
+//  render() {
+//    const greeting = "Hi!";    
+//    return (
+//      <React.Fragment>
+//        <label htmlFor="bar">bar</label>
+//        <input type="text" onChange={() => console.log(1)} />
+//      </React.Fragment>
+//    )
+//  }
+//}
 
 export default App;
