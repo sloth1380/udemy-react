cd7c414074c32c63ec815e4109065e30e460c36e
miura <sloth1380.info@gmail.com>
2021-10-23 00:02:59 +0900

    implement event new component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,15 @@
 import axios from 'axios'
 
-
 export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+export const READ_EVENTS = 'READ_EVENTS';
+export const POST_EVENTS = 'POST_EVENTS';
+
 export const increment = () => ({
     type: 'INCREMENT'
 })
 
-export const DECREMENT = 'DECREMENT';
 export const decrement = () => ({
     type: 'DECREMENT'
 })
@@ -14,9 +17,14 @@ export const decrement = () => ({
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
 
-export const READ_EVENTS =  'READ_EVENTS';
 export const readEvents = () => async dispatch => {
     const response = await axios.get(`${ ROOT_URL }/events${ QUERYSTRING }`)
     //console.log(response)
     dispatch({type: READ_EVENTS, response})
+}
+
+export const postEvent = values => async dispatch => {
+    const response = await axios.post(`${ ROOT_URL }/events${ QUERYSTRING }`, values)
+
+    dispatch({ type: POST_EVENTS, response })
 }
--- src/compornents/events_new.js
+++ src/compornents/events_new.js
@@ -1,23 +1,61 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 
-//import { postEvent } frm '../actions';
+import { postEvent } from '../actions';
 
 class EventsNew extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div>
-            aaa
-        </div>
-      </React.Fragment>
-    )
-  }
+    constructor(props) {
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+
+    renderField(field) {
+        const { input, label, type, meta: { touched, error } } = field
+
+        return(
+            <div>
+                <input {...input} placeholder={label} type={type}/>
+                {touched && error && <span>{ error }</span>}
+            </div>
+        )
+    }
+
+    async onSubmit(values) {
+        await this.props.postEvent(values)
+        this.props.history.push('/')
+    }
+
+    render() {
+        const { handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit)}>
+                <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
+                <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
+                <div>
+                    <input type="submit" value="Submit" disabled={false} />
+                    <Link to="/" >Cancel</Link>
+                </div>
+            </form>
+        )
+    }
 }
 
 //const mapStateToProps = state => ({ events: state.events })
 
-//const mapDispatchToProps = ({ readEvents })
+const mapDispatchToProps = ({ postEvent })
+const validate = values => {
+    const errors = {}
 
-export default connect(null, null)(EventsNew);
+    if (!values.title) errors.title = "Enter a title, please."
+    if (!values.body) errors.body = "Enter a body, please."
+
+    return errors
+}
+
+export default connect(null, mapDispatchToProps)(
+    reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+)
